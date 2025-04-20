a53fb72facb70a44933a1660ab06e1dec6d60883
sergioperez <sermanaha@gmail.com>
2025-04-20 09:36:27 -0500

    Commit 4 : Configuracion de Servicios y Controlador

---
+++ Controllers/EdificioController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Controllers;
+
+use Models\Services\EdificioService;
+
+class EdificioController
+{
+    private EdificioService $edificioService;
+
+    public function __construct()
+    {
+        $this->edificioService = new EdificioService();
+    }
+
+
+    public function crear(array $datos)
+    {
+        try {
+
+            $edificio = new Edificio(
+                null,
+                $datos['nombre'],
+                floatval($datos['metrosCuadrados']),
+                floatval($datos['altura']),
+                intval($datos['numPisos']),
+                intval($datos['numApartamentos']),
+                intval($datos['numOficinas']),
+                $datos['nomParqueadero'] ?? null,
+                intval($datos['numPiscinas']),
+                intval($datos['tieneAscensor']),
+                floatval($datos['valorAdministracion']),
+                intval($datos['tieneZonaSocial']),
+                intval($datos['municipio_id']),
+                $datos['direccionExacta']
+            );
+
+
+            $this->edificioService->createEdificio($edificio);
+
+            echo "Edificio creado correctamente.";
+        } catch (Exception $e) {
+            echo "Error al crear el edificio: " . $e->getMessage();
+        }
+    }
+
+    public function actualizar(array $datos)
+    {
+
+        $edificio = new Edificio(
+            null,
+            $datos['nombre'],
+            floatval($datos['metrosCuadrados']),
+            floatval($datos['altura']),
+            intval($datos['numPisos']),
+            intval($datos['numApartamentos']),
+            intval($datos['numOficinas']),
+            $datos['nomParqueadero'] ?? null,
+            intval($datos['numPiscinas']),
+            intval($datos['tieneAscensor']),
+            floatval($datos['valorAdministracion']),
+            intval($datos['tieneZonaSocial']),
+            intval($datos['municipio_id']),
+            $datos['direccionExacta']
+        );
+        $edificio->setId((int)$datos['id']);
+
+        $this->edificioService->updateEdificio($edificio);
+        echo "Edificio actualizado correctamente.";
+
+    }
+
+    public function listar()
+    {
+        try {
+            $edificios = $this->edificioService->getAllEdificios();
+            header('Content-Type: application/json');
+            echo json_encode($edificios);
+        } catch (Exception $e) {
+            echo "Error al listar edificios: " . $e->getMessage();
+        }
+    }
+
+    public function verPorId(int $id)
+    {
+        try {
+            $edificio = $this->edificioService->getEdificioById($id);
+            if ($edificio) {
+                header('Content-Type: application/json');
+                echo json_encode($edificio);
+            } else {
+                echo "No se encontró un edificio con el ID proporcionado.";
+            }
+        } catch (Exception $e) {
+            echo "Error al obtener el edificio: " . $e->getMessage();
+        }
+    }
+}
---
+++ Models/Services/EdificioService.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Models\Services;
+
+use Models\Entity\Edificio;
+use Models\Repository\EdificioRepository;
+use Exception;
+class EdificioService
+{
+    private EdificioRepository $edificioRepository;
+
+    public function __construct()
+    {
+        $this->edificioRepository = new EdificioRepository();
+    }
+
+    public function getAllEdificios(): array
+    {
+        return $this->edificioRepository->findAll();
+    }
+
+    public function getEdificioById(int $id): ?Edificio
+    {
+        return $this->edificioRepository->findById($id);
+    }
+
+    public function createEdificio(Edificio $edificio)
+    {
+        if ($edificio->getId() != null) {
+            throw new Exception("El id del edificio NO debe ser asignado manualmente.");
+        }
+        if (empty($edificio->getNombre()) || $this->edificioRepository->findByName($edificio->getNombre()) != null) {
+            throw new Exception("El nombre del edificio no es válido o ya existe.");
+        }
+
+        if ($edificio->getMetrosCuadrados() <= 0) {
+            throw new Exception("Los metros cuadrados deben ser mayores a 0.");
+        }
+
+        if ($edificio->getAltura() <= 0) {
+            throw new Exception("La altura debe ser mayor a 0.");
+        }
+
+        if ($edificio->getNumPisos() < 1) {
+            throw new Exception("El número de pisos debe ser al menos 1.");
+        }
+
+        if ($edificio->getNumApartamentos() < 0) {
+            throw new Exception("El número de apartamentos no puede ser negativo.");
+        }
+
+        if ($edificio->getNumOficinas() < 0) {
+            throw new Exception("El número de oficinas no puede ser negativo.");
+        }
+
+        if ($edificio->getNumPiscinas() < 0) {
+            throw new Exception("El número de piscinas no puede ser negativo.");
+        }
+
+        if ($edificio->getValorAdministracion() < 0) {
+            throw new Exception("El valor de administración no puede ser negativo.");
+        }
+
+        if (!is_bool($edificio->getAscensor())) {
+            throw new Exception("El valor de ascensor debe ser booleano (true o false).");
+        }
+
+        if (!is_bool($edificio->getZonaSocial())) {
+            throw new Exception("El valor de zona social debe ser booleano (true o false).");
+        }
+
+        if ($edificio->getMunicipioId() <= 0) {
+            throw new Exception("Debe seleccionarse un municipio válido.");
+        }
+
+        if (empty($edificio->getDireccionExacta())) {
+            throw new Exception("La dirección exacta es obligatoria.");
+        }
+
+        return $this->edificioRepository->save($edificio);
+    }
+
+    public function updateEdificio(Edificio $edificio)
+    {
+        if($this->edificioRepository->findById($edificio->getId()) == null){
+            throw new Exception("El edificio no existe.");
+        }
+
+        return $this ->edificioRepository->update($edificio);
+    }
+
+    public function deleteEdificio(int $id): bool
+    {
+        if ($this->edificioRepository->findById($id) === null) {
+            throw new Exception("El edificio con ID $id no existe.");
+        }
+
+        return $this->edificioRepository->delete($id);
+    }
+
+
+}
+
+
